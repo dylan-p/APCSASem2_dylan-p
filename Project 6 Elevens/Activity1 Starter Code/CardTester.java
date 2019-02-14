579ed83899e3ff659fbebc70aecb925d90286625
dylan-p <31372610+dylan-p@users.noreply.github.com>
2019-02-14 10:22:23 -0800

    oklo

---
+++ Project 6 Elevens/Activity1 Starter Code/CardTester.java
@@ -0,0 +1,15 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card card = new Card("dig", "bick", 1);
+		Card cardT = new Card("gid", "kcib", 2);
+		Card cardTh = new Card("guu", "wuuu", 3);
+	}
+}
