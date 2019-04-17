e938189edd4172771b6bb67f7b9a0a8d01ffe921
Dylan Pratt <dylanpratt@Dylans-MacBook-Air.local>
2019-04-16 23:37:02 -0700

    E

--- DIG BICK/Cell.java
+++ DIG BICK/Cell.java
@@ -1,4 +1,26 @@
-public class Cell
+import java.util.*;
+public class Cell extends GridTester
 {
-   
+    private boolean occupied;
+    private int id;
+    private ArrayList<Cell> neighbors;
+
+    public void loadNeighbors(){
+        for(int i=1; i < arr.length-1; i++){
+            for(int j=1; j<arr[i].length-1; j++){
+                if(arr[i-1][j] == null){
+                    neighbors.add(arr[i-1][j]);
+                }
+                if(arr[i][j-1] == null){
+                    neighbors.add(arr[i][j-1]);
+                }
+                if(arr[i+1][j] == null){
+                    neighbors.add(arr[i+1][j]);
+                }
+                if(arr[i][j+1] == null){
+                    neighbors.add(arr[i][j+1]);
+                }
+            }
+        }
+    }
 }
